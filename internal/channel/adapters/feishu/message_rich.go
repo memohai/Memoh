41144d94239cad39eac350bda3c1567d8047bc10
ChrAlpha <53332481+ChrAlpha@users.noreply.github.com>
2026-06-14 17:10:03 +0000

    fix(channel): encode feishu link URL paren/angle brackets

--- internal/channel/adapters/feishu/message_rich.go
+++ internal/channel/adapters/feishu/message_rich.go
@@ -166,6 +166,9 @@ func escapeFeishuLinkURL(url string) string {
 	url = strings.ReplaceAll(strings.TrimSpace(url), "\n", "")
 	url = strings.ReplaceAll(url, "\r", "")
 	url = strings.ReplaceAll(url, " ", "%20")
+	url = strings.ReplaceAll(url, "<", "%3C")
+	url = strings.ReplaceAll(url, ">", "%3E")
+	url = strings.ReplaceAll(url, "(", "%28")
 	url = strings.ReplaceAll(url, ")", "%29")
 	return url
 }
